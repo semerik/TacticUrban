8496b444a9914875549ee13d3ee3d60be5cb1392
semerik <edwin-valero@upc.edu.co>
2023-09-28 14:20:34 -0500

    Cambio de todo el diseño y añadir main de navegacion

--- app/src/main/java/com/example/tacticurban/MainActivity.java
+++ app/src/main/java/com/example/tacticurban/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.tacticurban;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private ProgressBar cargando;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargando = findViewById(R.id.loadingProgressBar);
+
+
+    }
+
+    public void goSingIn(View view){
+        cargando.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(MainActivity.this, SignupForm.class);
+        startActivity(intent);
+    }
+
+    public void goLogin (View view){
+        cargando.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(MainActivity.this, LoginForm.class);
+        startActivity(intent);
     }
 
 }
